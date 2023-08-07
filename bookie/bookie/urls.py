22c5b9d10b5d2174d6bf7d71b046e39a66cb1984
Rafael Ottersberg <rafael.ottersberg@gmx.ch>
2023-08-07 09:00:18 +0200

    added adminshell

--- bookie/bookie/urls.py
+++ bookie/bookie/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('flightbook/', include("flightbook.urls")),
     path('', include('finance.urls')),
+    path('admin/shell/', include('django_admin_shell.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', admin.site.urls),
 ]
